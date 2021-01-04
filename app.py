8179cbfabcd0e519cb6a7231aed9a11369d9e3d1
Justin Schellenberg <jrschellenberg@gmail.com>
2021-01-04 13:43:08 -0600

    Updating Routes

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, flash, request
+from services import JournalService
+
+
 app = Flask(__name__)
-
-
-@app.route('/index', methods=['GET'])
-def index():
-    return redirect('/')
+app.secret_key = "my_not_so_safe_secret_key_since_this_is_development"
 
 
 @app.route('/', methods=['GET'])
 def home():
+    return redirect('/entries')
+
+
+@app.route('/entries', methods=['GET'])
+def index():
     return render_template('index.html')
 
 
-@app.route('/detail', methods=['GET'])
-def detail():
+@app.route('/entries/<journal_id>/delete', methods=['DELETE', 'GET'])
+def delete_entry(journal_id):
+    print(f'Journal id {journal_id}')
+    JournalService.delete_record(journal_id)
+    return redirect('/entries')
+
+
+@app.route('/entries/new', methods=['POST'])
+def create():
+    print(request.form)
+
+
+@app.route('/entries/<journal_id>', methods=['GET'])
+def detail(journal_id):
+    journal, is_error = JournalService.get_record_by_id(journal_id)
+    if is_error:
+        flash('Journal Not Found!', 'error')
+        return redirect('/')
     return render_template('detail.html')
 
 
@@ -28,4 +48,5 @@ def new():
 
 
 if __name__ == '__main__':
+    JournalService.import_database_by_csv('storage/seed.csv')
     app.run(debug=True, port=5000, host="0.0.0.0")
